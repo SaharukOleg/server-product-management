1d9d7e50c35dec6751bcd3d9bf31aff5d5546d5d
osakh <saharukoleg1@gmail.com>
2019-12-23 19:19:28 +0200

    create User Service Implementation with few CRUD methods

---
+++ src/main/java/com/sha/serverproductmanagement/service/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.sha.serverproductmanagement.service;
+
+import com.sha.serverproductmanagement.model.User;
+import com.sha.serverproductmanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    // describe it later on web security config.
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    //save can be used for update and insert operation in Database
+    @Override
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+
+    @Override
+    public List<User> findAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Long numberOfUsers(){
+        return userRepository.count();
+    }
+}
