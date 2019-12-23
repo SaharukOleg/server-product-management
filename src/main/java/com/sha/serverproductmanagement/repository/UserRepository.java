3679f0eea334dde00b6ddc83e8e49f60f172ce0a
osakh <saharukoleg1@gmail.com>
2019-12-23 14:40:49 +0200

    create Repository for User

---
+++ src/main/java/com/sha/serverproductmanagement/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.sha.serverproductmanagement.repository;
+
+import com.sha.serverproductmanagement.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
