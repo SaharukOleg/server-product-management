5df88d211ff4f55afc68476e09b0376cc27192af
osakh <saharukoleg1@gmail.com>
2019-12-24 17:19:18 +0200

    User Datails Service implementation Security

--- src/main/java/com/sha/serverproductmanagement/repository/UserRepository.java
+++ src/main/java/com/sha/serverproductmanagement/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.sha.serverproductmanagement.repository;
 import com.sha.serverproductmanagement.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
