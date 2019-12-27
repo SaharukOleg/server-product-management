51b8024f8ba5a1ee24c1078a4a6754dc76949e33
osakh <saharukoleg1@gmail.com>
2019-12-27 16:29:30 +0200

    fix

--- src/main/java/com/sha/serverproductmanagement/service/UserServiceImpl.java
+++ src/main/java/com/sha/serverproductmanagement/service/UserServiceImpl.java
@@ -18,24 +18,24 @@ public class UserServiceImpl implements UserService {
     private PasswordEncoder passwordEncoder;
 
     @Override
-    public User saveUser(User user) {
+    public User saveUser(final User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
     //save can be used for update and insert operation in Database
     @Override
-    public User updateUser(User user) {
+    public User updateUser(final User user) {
         return userRepository.save(user);
     }
 
     @Override
-    public void deleteUser(Long userId) {
+    public void deleteUser(final Long userId) {
         userRepository.deleteById(userId);
     }
 
     @Override
-    public User findByUsername(String username) {
+    public User findByUsername(final String username) {
         return userRepository.findByUsername(username).orElse(null);
     }
 
